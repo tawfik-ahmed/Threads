8c0bc51cc66ddb93eafe93970d5feabe9d8ada60
Tawfik Ahmed <tawfikahmed1x@gmail.com>
2024-12-07 23:03:39 +0200

    fix communities page not found

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -13,6 +13,8 @@ interface Props {
 
 const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
   const router = useRouter();
+  const isCommunity = personType === "Community";
+
   return (
     <article className="user-card">
       <div className="user-card_avatar">
@@ -32,7 +34,13 @@ const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
 
       <Button
         className="user-card_btn"
-        onClick={() => router.push(`/profile/${id}`)}
+        onClick={() => {
+          if (isCommunity) {
+            router.push(`/communities/${id}`);
+          } else {
+            router.push(`/profile/${id}`);
+          }
+        }}
       >
         View
       </Button>
